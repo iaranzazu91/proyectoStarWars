3d5bc32cda8ae6595ab2d4da721304cd5f8eb1f6
iaranzazu91 <i.aranzazu91@gmail.com>
2018-04-16 18:07:24 -0400

    listo papa

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -12,7 +12,7 @@
       <div class="row">
 
         <div class="col-md-8">
-          <img class="img-fluid" src="http://placehold.it/750x500" alt="">
+          <img src="posters/starwars.jpg" height="600" width="700">
         </div>
 
         <div class="col-md-4">
@@ -31,33 +31,32 @@
       <!-- /.row -->
 
       <!-- Related Projects Row -->
-      <h3 class="my-4">Related Projects</h3>
+      <h3 class="my-4">Creadores | Productores | Escritores</h3>
 
       <div class="row">
 
         <div class="col-md-3 col-sm-6 mb-4">
           <a href="#">
-            <img class="img-fluid" src="http://placehold.it/500x300" alt="">
+            <img class="img-fluid" src="posters/george_lucas.jpg" height="500" width="300" >
+            <p>George Lucas</p>
           </a>
         </div>
 
         <div class="col-md-3 col-sm-6 mb-4">
           <a href="#">
-            <img class="img-fluid" src="http://placehold.it/500x300" alt="">
+            <img class="img-fluid" src="posters/rick_mccallum.jpg" height="500" width="300">
+            <p> Rick McCallum</p>
           </a>
         </div>
 
         <div class="col-md-3 col-sm-6 mb-4">
           <a href="#">
-            <img class="img-fluid" src="http://placehold.it/500x300" alt="">
+            <img class="img-fluid" src="posters/Jonathan_Hales.jpg" height="500" width="300">
+            <p>Jonathan Hales</p>
           </a>
         </div>
 
-        <div class="col-md-3 col-sm-6 mb-4">
-          <a href="#">
-            <img class="img-fluid" src="http://placehold.it/500x300" alt="">
-          </a>
-        </div>
+
 
 
 
